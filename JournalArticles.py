2f5762fec84edfe71c1c0e2bd7525b0118854ba4
harej <jamesmhare@gmail.com>
2017-03-16 18:44:37 -0700

    Moving around imports, once more

--- JournalArticles.py
+++ JournalArticles.py
@@ -2,6 +2,9 @@ import arrow
 import html
 import requests
 from wikidataintegrator import wdi_core, wdi_login
+from .site_credentials import *
+
+WIKI_SESSION = wdi_login.WDLogin(user=site_username, pwd=site_password)
 
 def bundle_maker(biglist, size=200):
     """
--- __init__.py
+++
@@ -1,4 +0,0 @@
-from .site_credentials import *
-from wikidataintegrator import wdi_login
-
-WIKI_SESSION = wdi_login.WDLogin(user=site_username, pwd=site_password)
